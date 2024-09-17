0b7c1f440076f590cdcf1004cc085cd6cdab80d6
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-09-18 05:04:00 +0800

    Add files via upload

---
+++ cha.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Set variables
+POOL_URL="stratum+tcp://172.81.133.6:4400"
+WORKER_PREFIX="akakchacha"
+MACHINE_NAME=$(hostname)
+WORKER_NAME="${WORKER_PREFIX}.${MACHINE_NAME}"
+
+# Auto-detect GPU count
+GPU_COUNT=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
+DEVICES=$(seq -s, 0 $((GPU_COUNT - 1)))
+
+# Build the final command
+COMMAND="./aleominer -u $POOL_URL -d $DEVICES -w $WORKER_NAME"
+
+# Output the final command
+echo "Running command: $COMMAND"
+
+# Run Aleo Miner
+$COMMAND
+
+# Check if Aleo Miner exits, if so, restart it
+while true; do
+    $COMMAND
+    if [ $? -ne 0 ]; then
+        echo "Aleo Miner has exited, restarting..."
+        sleep 5
+    else
+        break
+    fi
+done
