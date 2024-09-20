4c4e485b903981ba5473a0145d2e1d0284902db3
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-09-20 12:54:44 +0800

    Add files via upload

--- cha.sh
+++ cha.sh
@@ -1,31 +1,55 @@
 #!/bin/bash
 
 # Set variables
-POOL_URL="stratum+tcp://172.81.133.6:4400"
-WORKER_PREFIX="akakchacha"
-MACHINE_NAME=$(hostname)
+POOL_URL="stratum+tcp://aleo-asia.f2pool.com:4400"
+WORKER_PREFIX="akton0208"
+HOSTNAME=$(hostname)
+GPU_MODEL=$(nvidia-smi --query-gpu=name --format=csv,noheader | head -n 1)
+GPU_MODEL_NUMBER=$(echo $GPU_MODEL | grep -oP '\d+')
+GPU_COUNT=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
+MACHINE_NAME="${HOSTNAME}-${GPU_MODEL_NUMBER}-${GPU_COUNT}"
 WORKER_NAME="${WORKER_PREFIX}.${MACHINE_NAME}"
 
 # Auto-detect GPU count
-GPU_COUNT=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
 DEVICES=$(seq -s, 0 $((GPU_COUNT - 1)))
 
-# Build the final command
-COMMAND="./aleominer -u $POOL_URL -d $DEVICES -w $WORKER_NAME"
+# Function to run Aleo Miner
+run_aleo_miner() {
+    COMMAND="./aleominer -u $POOL_URL -d $DEVICES -w $WORKER_NAME"
+    echo "Running command: $COMMAND"
+    while true; do
+        $COMMAND
+        if [ $? -ne 0 ]; then
+            echo "Aleo Miner has exited, restarting..."
+            sleep 5
+        else
+            break
+        fi
+    done
+}
 
-# Output the final command
-echo "Running command: $COMMAND"
+# Function to run Aleo Prover
+run_aleo_prover() {
+    cd /root/aleo_prover
+    COMMAND="./aleo_prover --pool aleo.hk.zk.work:10003 --address aleo16vqvtd0kr2fupv5rahhxw3hfyc9dc63k6447lee7z4y5ezp4gqys6un25m --custom_name $MACHINE_NAME"
+    echo "Running command: $COMMAND"
+    while true; do
+        $COMMAND
+        if [ $? -ne 0 ]; then
+            echo "Aleo Prover has exited, restarting..."
+            sleep 5
+        else
+            break
+        fi
+    done
+}
 
-# Run Aleo Miner
-$COMMAND
-
-# Check if Aleo Miner exits, if so, restart it
-while true; do
-    $COMMAND
-    if [ $? -ne 0 ]; then
-        echo "Aleo Miner has exited, restarting..."
-        sleep 5
-    else
-        break
-    fi
-done
+# Check the argument and run the corresponding function
+if [ "$1" == "f2" ]; then
+    run_aleo_miner
+elif [ "$1" == "zk" ]; then
+    run_aleo_prover
+else
+    echo "Usage: $0 {f2|zk}"
+    exit 1
+fi
