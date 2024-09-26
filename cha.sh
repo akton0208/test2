b55c7cc05d351a5ecd2d8b35578edb632c9ec5f9
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-09-26 15:18:27 +0800

    Update cha.sh

--- cha.sh
+++ cha.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 # Set variables
-POOL_URL="stratum+tcp://aleo-asia.f2pool.com:4400"
-WORKER_PREFIX="akton0208"
 HOSTNAME=$(hostname)
 GPU_MODEL=$(nvidia-smi --query-gpu=name --format=csv,noheader | head -n 1)
 GPU_MODEL_NUMBER=$(echo $GPU_MODEL | grep -oP '\d+')
 GPU_COUNT=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
 MACHINE_NAME="${HOSTNAME}-${GPU_MODEL_NUMBER}-${GPU_COUNT}"
-WORKER_NAME="${WORKER_PREFIX}.${MACHINE_NAME}"
 
 # Auto-detect GPU count
 DEVICES=$(seq -s, 0 $((GPU_COUNT - 1)))
@@ -17,8 +14,12 @@ DEVICES=$(seq -s, 0 $((GPU_COUNT - 1)))
 run_aleo_miner() {
     # Download Aleo Miner
     wget -O aleominer https://raw.githubusercontent.com/akton0208/test2/main/aleominer
+    if [ $? -ne 0 ]; then
+        echo "Failed to download Aleo Miner"
+        exit 1
+    fi
     chmod +x aleominer
-    COMMAND="./aleominer -u $POOL_URL -d $DEVICES -w $WORKER_NAME"
+    COMMAND="./aleominer -u stratum+tcp://aleo-asia.f2pool.com:4400 -d $DEVICES -w $MACHINE_NAME"
     echo "Running command: $COMMAND"
     while true; do
         $COMMAND
@@ -34,6 +35,10 @@ run_aleo_miner() {
 # Function to run Aleo Prover
 run_aleo_prover() {
     wget -O aleo_prover-v0.2.1.tar.gz https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/v0.2.1/aleo_prover-v0.2.1.tar.gz
+    if [ $? -ne 0 ]; then
+        echo "Failed to download Aleo Prover"
+        exit 1
+    fi
     tar -vxf aleo_prover-v0.2.1.tar.gz     
     cd /root/aleo_prover
     COMMAND="./aleo_prover --pool aleo.hk.zk.work:10003 --address aleo16vqvtd0kr2fupv5rahhxw3hfyc9dc63k6447lee7z4y5ezp4gqys6un25m --custom_name $MACHINE_NAME"
@@ -49,12 +54,35 @@ run_aleo_prover() {
     done
 }
 
+# Function to run Oula Prover
+run_oula_prover() {
+    wget -O oula-pool-prover https://github.com/oula-network/aleo/releases/download/v1.12/oula-pool-prover
+    if [ $? -ne 0 ]; then
+        echo "Failed to download Oula Prover"
+        exit 1
+    fi
+    chmod +x oula-pool-prover
+    COMMAND="./oula-pool-prover --pool wss://aleo.oula.network:6666 --account akcha --worker-name $MACHINE_NAME"
+    echo "Running command: $COMMAND"
+    while true; do
+        $COMMAND
+        if [ $? -ne 0 ]; then
+            echo "Oula Prover has exited, restarting..."
+            sleep 5
+        else
+            break
+        fi
+    done
+}
+
 # Check the argument and run the corresponding function
 if [ "$1" == "f2" ]; then
     run_aleo_miner
 elif [ "$1" == "zk" ]; then
     run_aleo_prover
+elif [ "$1" == "oula" ]; then
+    run_oula_prover
 else
-    echo "Usage: $0 {f2|zk}"
+    echo "Usage: $0 {f2|zk|oula}"
     exit 1
 fi
