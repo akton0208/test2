e1e3ee987275d63497e3511b600352e32a01df89
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-09-27 22:20:38 +0800

    Update aleooula.sh

--- aleooula.sh
+++ aleooula.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+gpu_info=$(nvidia-smi --query-gpu=name --format=csv,noheader)
+gpu_count=$(echo "$gpu_info" | wc -l)
+gpu_model=$(echo "$gpu_info" | head -n 1 | grep -oP '\d{4}')  # 提取型號中的數字部分
+
+vastname=$(cat ~/.vast_containerlabel)
+vastname_last8=$(echo "$vastname" | tail -c 9)  # 包括前面的 "_" 符號
+MACHINE="${gpu_count}X${gpu_model}_${vastname_last8}"
+
 # Download and extract the miner
 wget -O oula-pool-prover https://github.com/oula-network/aleo/releases/download/v1.12/oula-pool-prover
 chmod +x oula-pool-prover
@@ -7,19 +15,8 @@ chmod +x oula-pool-prover
 # Check if the address parameter is provided, set default if not
 address=${1:-akcha}
 
-# Get GPU model and count
-gpu_info=$(nvidia-smi --query-gpu=name --format=csv,noheader)
-gpu_count=$(echo "$gpu_info" | wc -l)
-gpu_model=$(echo "$gpu_info" | head -n 1 | grep -oP '\d{4}')  # Extract the numeric part of the model
-
-# Get the machine name
-hostname=$(hostname)
-
-# Format as "countXmodel_hostname"
-gpu_summary="${gpu_count}X${gpu_model}_${hostname}"
-
 # Define your command
-command="./oula-pool-prover --pool wss://aleo.oula.network:6666 --account $address --worker-name $gpu_summary"
+command="./oula-pool-prover --pool wss://aleo.oula.network:6666 --account $address --worker-name $MACHINE"
 
 # Display the final command
 echo "Final command: $command"
