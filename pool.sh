12b3dcc64eda808f55c70bfd0302e30331fb9cc2
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-09-28 02:30:11 +0800

    Update pool.sh

--- pool.sh
+++ pool.sh
@@ -10,23 +10,23 @@ wallet_address=${1:-$default_wallet_address}
 total_threads=$(nproc)
 threads_per_task=96
 
-# If the total number of threads is less than the threads required per task, use all available threads
-if [ $total_threads -lt $threads_per_task ]; then
-  threads_per_task=$total_threads
-fi
+# If the total number of threads is less than or equal to the threads required per task, run without taskset
+if [ $total_threads -le $threads_per_task ]; then
+  ./ore-mine-pool-linux worker --route-server-url 'http://47.254.182.83:8080/' --server-url direct --worker-wallet-address $wallet_address &
+else
+  # Calculate the number of tasks
+  num_tasks=$((total_threads / threads_per_task))
+  remaining_threads=$((total_threads % threads_per_task))
 
-# Calculate the number of tasks
-num_tasks=$((total_threads / threads_per_task))
-remaining_threads=$((total_threads % threads_per_task))
+  # Create tasks
+  for ((i=0; i<num_tasks; i++)); do
+    taskset -c $((i * threads_per_task))-$(((i + 1) * threads_per_task - 1)) ./ore-mine-pool-linux worker --route-server-url 'http://47.254.182.83:8080/' --server-url direct --worker-wallet-address $wallet_address &
+  done
 
-# Create tasks
-for ((i=0; i<num_tasks; i++)); do
-  taskset -c $((i * threads_per_task))-$(((i + 1) * threads_per_task - 1)) ./ore-mine-pool-linux worker --route-server-url 'http://47.254.182.83:8080/' --server-url direct --worker-wallet-address $wallet_address &
-done
-
-# If there are remaining threads, create the last task
-if [ $remaining_threads -ne 0 ]; then
-  taskset -c $((num_tasks * threads_per_task))-$((total_threads - 1)) ./ore-mine-pool-linux worker --route-server-url 'http://47.254.182.83:8080/' --server-url direct --worker-wallet-address $wallet_address &
+  # If there are remaining threads, create the last task
+  if [ $remaining_threads -ne 0 ]; then
+    taskset -c $((num_tasks * threads_per_task))-$((total_threads - 1)) ./ore-mine-pool-linux worker --route-server-url 'http://47.254.182.83:8080/' --server-url direct --worker-wallet-address $wallet_address &
+  fi
 fi
 
 # Wait for all tasks to complete
