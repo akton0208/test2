5b51a41620079c05ba791e5150c6b1124b0600bd
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-09-27 22:24:59 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-# 獲取 GPU 型號和數量
+# Get GPU model and count
 gpu_info=$(nvidia-smi --query-gpu=name --format=csv,noheader)
 gpu_count=$(echo "$gpu_info" | wc -l)
-gpu_model=$(echo "$gpu_info" | head -n 1 | grep -oP '\d{4}')  # 提取型號中的數字部分
+gpu_model=$(echo "$gpu_info" | head -n 1 | grep -oP '\d{4}')  # Extract the numeric part of the model
 
-# 獲取機器名稱並提取最後 8 位數字
+# Get the machine name and extract the last 8 digits
 vastname=$(cat ~/.vast_containerlabel)
-vastname_last8=$(echo "$vastname" | tail -c 9)  # 包括前面的 "_" 符號
+vastname_last8=$(echo "$vastname" | tail -c 9)  # Includes the preceding "_"
 
-# 格式化為 "countXmodel_hostname"
+# Format as "countXmodel_hostname"
 MACHINE="${gpu_count}X${gpu_model}_${vastname_last8}"
-echo "MACHINE is set to: $MACHINE"  # 檢查變量設置
+echo "MACHINE is set to: $MACHINE"  # Check the variable setting
 export MACHINE
 
-# 基本 URL 和文件列表
+# Base URL and file list
 BASE_URL="https://raw.githubusercontent.com/akton0208/test2/main/"
 FILES=("aleof2.sh" "aleozk.sh" "f2q.sh" "aleooula.sh" "pool.sh" "oulaq.sh")
 
-# 下載和設置每個文件
+# Download and set up each file
 for FILE in "${FILES[@]}"; do
     wget -O $FILE ${BASE_URL}${FILE}
     if [ $? -ne 0 ]; then
