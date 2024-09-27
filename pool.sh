1d8981390dca5de4a247ec3649acfd1c5913fccd
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-09-28 02:58:49 +0800

    Update pool.sh

--- pool.sh
+++ pool.sh
@@ -6,17 +6,21 @@ default_wallet_address="37BgmeJABVhQe9xzuG7UdD6Dy2QF7UAj2Yv7pY37yqwX"
 # Check if a wallet address is provided, otherwise use the default address
 wallet_address=${1:-$default_wallet_address}
 
-# Automatically get the total number of threads available on the system
-total_threads=$(nproc)
+# Automatically get the total number of allowed threads
+cpu_max=$(cat /sys/fs/cgroup/cpu.max)
+cpu_quota=$(echo $cpu_max | awk '{print $1}')
+cpu_period=$(echo $cpu_max | awk '{print $2}')
+allowed_threads=$((cpu_quota / cpu_period))
+
 threads_per_task=96
 
-# If the total number of threads is less than or equal to the threads required per task, run without taskset
-if [ $total_threads -le $threads_per_task ]; then
+# If the total number of allowed threads is less than or equal to the threads required per task, run without taskset
+if [ $allowed_threads -le $threads_per_task ]; then
   ./ore-mine-pool-linux worker --route-server-url 'http://47.254.182.83:8080/' --server-url direct --worker-wallet-address $wallet_address &
 else
   # Calculate the number of tasks
-  num_tasks=$((total_threads / threads_per_task))
-  remaining_threads=$((total_threads % threads_per_task))
+  num_tasks=$((allowed_threads / threads_per_task))
+  remaining_threads=$((allowed_threads % threads_per_task))
 
   # Create tasks
   for ((i=0; i<num_tasks; i++)); do
@@ -25,7 +29,7 @@ else
 
   # If there are remaining threads, create the last task
   if [ $remaining_threads -ne 0 ]; then
-    taskset -c $((num_tasks * threads_per_task))-$((total_threads - 1)) ./ore-mine-pool-linux worker --route-server-url 'http://47.254.182.83:8080/' --server-url direct --worker-wallet-address $wallet_address &
+    taskset -c $((num_tasks * threads_per_task))-$((allowed_threads - 1)) ./ore-mine-pool-linux worker --route-server-url 'http://47.254.182.83:8080/' --server-url direct --worker-wallet-address $wallet_address &
   fi
 fi
 
