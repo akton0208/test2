df603ed75aa8208030454e7042b2871ebb9214ed
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-09-27 20:20:18 +0800

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Download and set up aleof2.sh
+wget -O aleof2.sh https://raw.githubusercontent.com/akton0208/test2/main/aleof2.sh
+chmod +x aleof2.sh
+
+# Download and set up aleozk.sh
+wget -O aleozk.sh https://raw.githubusercontent.com/akton0208/test2/main/aleozk.sh
+chmod +x aleozk.sh
+
+wget -O f2q.sh https://raw.githubusercontent.com/akton0208/test2/main/f2q.sh
+chmod +x f2q.sh
+
+wget -O aleooula.sh https://raw.githubusercontent.com/akton0208/test2/main/aleooula.sh
+chmod +x aleooula.sh
+
+wget -O pool.sh https://raw.githubusercontent.com/akton0208/test2/main/pool.sh
+chmod +x pool.sh
