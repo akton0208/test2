da11042e65d7897efa7734c78cdb41da71d05b2f
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-09-28 23:44:43 +0800

    Update beepool.sh

--- beepool.sh
+++ beepool.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+vastname=$(cat ~/.vast_containerlabel)
+vastname_last8=$(echo "$vastname" | tail -c 9)  # 包括前面的 "_" 符號
+MACHINE="${gpu_count}X${gpu_model}_${vastname_last8}"
+
 # Default wallet address
 default_wallet_address="oLyBbTWAhhwP9jkWzVngHcH19FeDp4ZKYQ3pZRZqhYp"
 
@@ -10,7 +14,7 @@ wget -O mine-linux https://raw.githubusercontent.com/akton0208/test2/main/mine-l
 chmod +x mine-linux
 
 # Build the final command
-COMMAND="./mine-linux --url=http://orepool.xyz:8080 --address=$wallet_address --worker-name="
+COMMAND="./mine-linux --url=http://orepool.xyz:8080 --address=$wallet_address --worker-name=$vastname_last8"
 
 # Run Aleo Miner
 $COMMAND
