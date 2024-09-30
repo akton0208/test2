b51cda6f6d6877e96e41c60423880f0310efbc00
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-09-30 16:06:58 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-wget -O setup.sh https://raw.githubusercontent.com/akton0208/test2/main/setup.sh
-if [ $? -ne 0 ]; then
-    echo "download fail"
-    exit 1
-fi
+# Base URL and file list
+BASE_URL="https://raw.githubusercontent.com/akton0208/test2/main/"
+FILES=("aksetup.sh")
 
-chmod +x setup.sh
-./setup.sh
+# Download and set up each file
+for FILE in "${FILES[@]}"; do
+    wget -O $FILE ${BASE_URL}${FILE}
+    if [ $? -ne 0 ]; then
+        echo "Failed to download $FILE"
+        exit 1
+    fi
+    chmod +x $FILE
+done
+
+./aksetup.sh
