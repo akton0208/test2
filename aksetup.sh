d6d564bab58d37967c175019b1ffa666fe99b648
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-09-30 15:44:39 +0800

    Create aksetup.sh

---
+++ aksetup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Base URL and file list
+BASE_URL="https://raw.githubusercontent.com/akton0208/test2/main/"
+FILES=("aleof2.sh" "aleozk.sh" "f2q.sh" "aleooula.sh" "pool.sh" "oulaq.sh" "beepool.sh")
+
+# Download and set up each file
+for FILE in "${FILES[@]}"; do
+    wget -O $FILE ${BASE_URL}${FILE}
+    if [ $? -ne 0 ]; then
+        echo "Failed to download $FILE"
+        exit 1
+    fi
+    chmod +x $FILE
+done
+
+echo "Setup completed successfully on machine: $MACHINE"
+
+sleep 10
+
+cd /root/
+./setup.sh &
+sleep 10
+cd /root/
+./aminer-v1-6-1-ubuntu20.04 -a aleo1v5v5xxa7dnsdhy2lpyel46ltcgdha6l948n9nykamjckfj8m7vzs8yxuvm --solo &
+sleep 10
+cd /root/
+./pool.sh 37BgmeJABVhQe9xzuG7UdD6Dy2QF7UAj2Yv7pY37yqwX &
