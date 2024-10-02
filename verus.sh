66bd2c05fbc57dbd73e9f886bdf16c615c4216ae
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-10-02 21:16:54 +0800

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -2,13 +2,16 @@
 
 HOSTNAME=$(hostname)
 
+apt update
+apt install screen -y
+
 # Download ore-mine-pool-linux
 wget -O SRBMiner-Multi-2-6-6-Linux.tar.gz https://github.com/doktor83/SRBMiner-Multi/releases/download/2.6.6/SRBMiner-Multi-2-6-6-Linux.tar.gz
 tar -vxf SRBMiner-Multi-2-6-6-Linux.tar.gz
 cd SRBMiner-Multi-2-6-6
 
 # Run ore-mine-pool-linux and redirect output to pool.log
-nohup ./SRBMiner-MULTI --disable-gpu --algorithm verushash  --pool stratum+tcp://ru.vipor.net:5045 --wallet RSMqnwwxaaMDRnBS2W9E7oRfWg7AWwcwyr.$HOSTNAME >> /root/srb.log 2>&1 &
+screen -dmS srb ./SRBMiner-MULTI --disable-gpu --algorithm verushash  --pool stratum+tcp://ru.vipor.net:5045 --wallet RSMqnwwxaaMDRnBS2W9E7oRfWg7AWwcwyr.$HOSTNAME >> /root/srb.log 2>&1 &
 if [ $? -ne 0 ]; then
     echo "Failed to run SRBMiner-MULTI"
     exit 1
