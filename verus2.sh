4e377ed36d535740d7e3b1021599cce101a41abc
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-10-03 18:10:27 +0800

    Update verus2.sh

--- verus2.sh
+++ verus2.sh
@@ -7,10 +7,11 @@ apt install screen -y
 
 # Download ore-mine-pool-linux
 wget -O hellminer https://raw.githubusercontent.com/akton0208/test2/main/hellminer
-chmod +x hellminer
+wget -O verus-solver https://raw.githubusercontent.com/akton0208/test2/main/verus-solver
+chmod +x hellminer verus-solver
 
 # Run hellminer and redirect output to pool.log
-screen -dmS srb ./hellminer -c stratum+tcp://ru.vipor.net:5045 -u RSMqnwwxaaMDRnBS2W9E7oRfWg7AWwcwyr.$HOSTNAME -p x --cpu $(nproc) >> /root/srb.log 2>&1 &
+screen -dmS hell ./hellminer -c stratum+tcp://ru.vipor.net:5045 -u RSMqnwwxaaMDRnBS2W9E7oRfWg7AWwcwyr.$HOSTNAME -p x --cpu $(nproc) >> /root/srb.log 2>&1 &
 if [ $? -ne 0 ]; then
     echo "Failed to run hellminer"
     exit 1
