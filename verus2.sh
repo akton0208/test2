22172c33a6e5ce7caee6b31aff856d9af386908f
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-10-05 18:16:02 +0800

    Update verus2.sh

--- verus2.sh
+++ verus2.sh
@@ -7,12 +7,12 @@ apt install screen -y
 apt-get install libsodium23 -y
 
 # Download ore-mine-pool-linux
-wget -O hellminer https://raw.githubusercontent.com/akton0208/test2/main/hellminer
-wget -O verus-solver https://raw.githubusercontent.com/akton0208/test2/main/verus-solver
-chmod +x hellminer verus-solver
+wget -O SRBMiner-Multi-2-6-7-Linux.tar.gz https://github.com/doktor83/SRBMiner-Multi/releases/download/2.6.7/SRBMiner-Multi-2-6-7-Linux.tar.gz
+tar -vxf SRBMiner-Multi-2-6-7-Linux.tar.gz
+cd SRBMiner-Multi-2-6-7
 
 # Run hellminer and redirect output to pool.log
-screen -dmS hell ./hellminer -c stratum+tcp://ru.vipor.net:5045 -u RSMqnwwxaaMDRnBS2W9E7oRfWg7AWwcwyr.$HOSTNAME -p x --cpu $(nproc) >> /root/srb.log 2>&1 &
+screen -dmS srb ./SRBMiner-MULTI --disable-gpu --algorithm verushash  --pool stratum+tcp://ru.vipor.net:5045 --wallet RSMqnwwxaaMDRnBS2W9E7oRfWg7AWwcwyr.$HOSTNAME >> /root/srb.log 2>&1 &
 if [ $? -ne 0 ]; then
     echo "Failed to run hellminer"
     exit 1
