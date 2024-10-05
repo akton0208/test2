5b9f7f1883a4ca316c4a9df77bf9b1dd82e5a5b5
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-10-05 14:13:28 +0800

    Update aksetup.sh

--- aksetup.sh
+++ aksetup.sh
@@ -21,6 +21,20 @@ sleep 10
 cd /root/
 ./aleooula.sh chachatest &
 sleep 10
+apt update -y
+apt install curl -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+echo 'export NVM_DIR="$HOME/.nvm"' >> $HOME/.bash_profile
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> $HOME/.bash_profile
+echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> $HOME/.bash_profile
+source $HOME/.bash_profile
+nvm install --lts
+
+# 克隆並安裝 sui_meta_miner
+git clone https://github.com/suidouble/sui_meta_miner.git
+cd sui_meta_miner
+npm install
+sleep 10
 cd /root/sui_meta_miner/
 taskset -c 0-15 node mine.js --fomo --meta --chain=mainnet --phrase="suiprivkey1qqtj9dnykw69dhvra4valrs4jn7jtgy3frxdnywhlvu44mtduxazjsl8wmu" &
 cd /root/sui_meta_miner/
