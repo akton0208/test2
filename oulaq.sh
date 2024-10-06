2c44268ce7bda7ff7e418627443e1463cd5276a6
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-10-06 15:47:36 +0800

    Update oulaq.sh

--- oulaq.sh
+++ oulaq.sh
@@ -9,7 +9,7 @@ vastname_last8=$(echo "$vastname" | tail -c 9)  # 包括前面的 "_" 符號
 MACHINE="${gpu_count}X${gpu_model}_${vastname_last8}"
 
 # Default parameters
-DEFAULT_ACCOUNT="akcha"
+DEFAULT_ACCOUNT="chachatest"
 DEFAULT_ACCESS_TOKEN="eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6ImZhNDNmNGJiLTM4YmEtNDAzMy04ZTBhLTRhODczNTc5YzlkZSIsIk1pbmluZyI6IiIsIm5iZiI6MTcyNjgxMDYxMCwiZXhwIjoxNzU4MzQ2NjEwLCJpYXQiOjE3MjY4MTA2MTAsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.s36mPmelvCmpwHyhfvOa3UwQdH4d2iBSri-8rIw9uTvdiPW3bVvk8HckZLb20orT14v5H_gRItH4p3qfXXr-jBpmnHfqsCoHaGiwF2HdeSEIAMkRzJhsFQ9IsRpMkxpvEh3jpP-jjQgq1eUXocu4Car49RnKawOG01hrxrWcoCam5wGcGbW2m67TEg2PDi4r92qfUnrccDNbYw7aJsg-VG9oZ3_k5HwtiaAIyHX-vqvoc1g26V8pmH1RExAU8RCT5QDopvmt84tngcWkQNgo6s3OcogkZ9_APn-Ul3GUVID6uVeJ_GCBl62ufp7ef4Anui5-iytmLfFXoiloBxb8mQ"
 
 # Use provided parameters or default values
