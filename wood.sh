5ce4f4f28a47f04eb49d51b61b9b7f9fbf1a19e2
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-10-07 16:02:29 +0800

    Update wood.sh

--- wood.sh
+++ wood.sh
@@ -1,31 +1,17 @@
 #!/bin/bash
 
 # 檢查是否提供了 --rpc 和 --key 參數
-while getopts ":r:k:" opt; do
-  case $opt in
-    r) RPC_URL="$OPTARG"
-    ;;
-    k) KEY="$OPTARG"
-    ;;
-    \?) echo "無效的選項 -$OPTARG" >&2
-        exit 1
-    ;;
-    :) echo "選項 -$OPTARG 需要一個參數" >&2
-       exit 1
-    ;;
-  esac
-done
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "參數不全，跳過創建 id.json 和執行 coal mine 命令"
+else
+  RPC_URL=$1
+  KEY=$2
 
-# 檢查是否提供了必要的參數
-if [ -z "$RPC_URL" ] || [ -z "$KEY" ]; then
-  echo "請提供 --rpc 和 --key 參數，例如：./wood.sh -r RPC_URL -k YOUR_KEY"
-  exit 1
-fi
+  # 在 /root 下創建 id.json 並寫入 key
+  echo "$KEY" > /root/id.json
 
-# 確保以 root 權限運行
-if [ "$EUID" -ne 0 ]; then
-  echo "請以 root 權限運行此腳本"
-  exit 1
+  # 執行 coal mine 命令
+  ./coal mine --resource wood --cores $(nproc) --keypair /root/id.json --rpc $RPC_URL --priority-fee 500
 fi
 
 # 更新軟件包列表並安裝必要的軟件包
@@ -36,13 +22,3 @@ cargo install coal-cli
 
 # 進入 .cargo/bin 目錄
 cd ~/.cargo/bin
-
-# 在 /root 下創建 id.json 並寫入 key
-if [ -n "$KEY" ]; then
-  echo "$KEY" > /root/id.json
-fi
-
-# 執行 coal mine 命令
-if [ -n "$RPC_URL" ] && [ -n "$KEY" ]; then
-  ./coal mine --resource wood --cores $(nproc) --keypair /root/id.json --rpc $RPC_URL --priority-fee 500
-fi
