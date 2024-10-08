1820b421293e1b60befd8bb0503e4d594fe0308f
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-10-08 19:48:06 +0800

    Update pool.sh

--- pool.sh
+++ pool.sh
@@ -46,7 +46,7 @@ while true; do
   echo "Running worker processes: $running_workers"
 done &
 
-threads_per_task=96
+threads_per_task=32
 
 # If the total number of allowed threads is less than or equal to the threads required per task, run without taskset
 if [ $allowed_threads -le $threads_per_task ]; then
