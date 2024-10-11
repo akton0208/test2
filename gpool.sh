ea912b383b8c890f7f61ff7b157d80ee6621681e
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-10-11 20:43:20 +0800

    Update gpool.sh

--- gpool.sh
+++ gpool.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
+# Default wallet address
+default_wallet_address="37BgmeJABVhQe9xzuG7UdD6Dy2QF7UAj2Yv7pY37yqwX"
+
+# Check if a wallet address is provided, otherwise use the default address
+wallet_address=${1:-$default_wallet_address}
+
 https://github.com/gpool-cloud/gpool-cli.git
 
-cd gpool-cli
+chmod -R +x gpool-cli
 
-chmod +x *
+# Build the final command
+COMMAND="./gpool --pubkey $wallet_address"
 
-./gpool --pubkey 37BgmeJABVhQe9xzuG7UdD6Dy2QF7UAj2Yv7pY37yqwX
+# Run 
+$COMMAND
+
+# Check if Miner exits, if so, restart it
+while true; do
+    $COMMAND
+    if [ $? -ne 0 ]; then
+        echo "Miner has exited, restarting..."
+        sleep 5
+    else
+        break
+    fi
+done
