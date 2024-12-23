4a0a8bb843bb7cbed86320a920647407ae3ddb31
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-12-23 22:20:30 +0800

    Update gpoolv.sh

--- gpoolv.sh
+++ gpoolv.sh
@@ -6,33 +6,23 @@ default_wallet_address="37BgmeJABVhQe9xzuG7UdD6Dy2QF7UAj2Yv7pY37yqwX"
 # Check if a wallet address is provided, otherwise use the default address
 wallet_address=${1:-$default_wallet_address}
 
-apt-get update
-apt-get install -y git
+# Update and install wget with error handling
+apt-get update && apt-get install -y wget || { echo "Failed to install wget"; exit 1; }
 
-if [ ! -d "gpool-cli" ]; then
-    git clone https://github.com/gpool-cloud/gpool-cli.git
+# Download the gpool binary if it doesn't exist
+if [ ! -f "gpool" ]; then
+    wget https://github.com/gpool-cloud/gpool-cli/releases/download/v2024.48.1/gpool || { echo "Failed to download gpool"; exit 1; }
 fi
 
-chmod +x gpool-cli/gpool
+chmod +x gpool
 
-# Build the final commands
-COMMAND1="./gpool-cli/gpool --pubkey $wallet_address"
-COMMAND2="./gpool-cli/gpool --pubkey $wallet_address --no-pcie"
+# Build the final command
+COMMAND="./gpool --pubkey $wallet_address"
 
-# Function to run the commands
+# Function to run the command
 run_command() {
-    $COMMAND1 || $COMMAND2
+    $COMMAND
 }
 
-# Run the command in the background
-run_command &
-
-# Restart every 15 minutes with time display
-while true; do
-    for ((i=15; i>0; i--)); do
-        echo "Restarting in $i minutes"
-        sleep 60  # Sleep for 1 minute
-    done
-    pkill -f gpool-cli
-    run_command &
-done
+# Run the command
+run_command
